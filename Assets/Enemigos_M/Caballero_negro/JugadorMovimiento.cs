0eacb2eb00a54015c8f9341cf77e179a9e7d10c0
Melquiel <melquielmoreno18@gmail.com>
2026-09-23 21:43:32 -0600

    listo

--- Assets/Enemigos_M/Caballero_negro/JugadorMovimiento.cs
+++ Assets/Enemigos_M/Caballero_negro/JugadorMovimiento.cs
@@ -6,27 +6,43 @@ public class JugadorMovimiento : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
     private Vector2 movimiento;
+    private Vector2 ultimaDireccion;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        
+        // Por defecto, mirar hacia abajo al empezar
+        ultimaDireccion = new Vector2(0, -1);
     }
 
     void Update()
     {
-        // Leer la entrada de las teclas A/D (X) y W/S (Y)
         movimiento.x = Input.GetAxisRaw("Horizontal");
         movimiento.y = Input.GetAxisRaw("Vertical");
 
-        // Mandar los valores al Blend Tree
-        animator.SetFloat("Horizontal", movimiento.x);
-        animator.SetFloat("Vertical", movimiento.y);
+        if (movimiento != Vector2.zero)
+        {
+            // Si nos estamos moviendo, actualizamos la última dirección
+            ultimaDireccion = movimiento;
+            
+            animator.SetBool("IsMoving", true);
+            animator.SetFloat("Horizontal", movimiento.x);
+            animator.SetFloat("Vertical", movimiento.y);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
+
+        // Enviamos la última dirección registrada para el Blend Tree de Idle
+        animator.SetFloat("LastHorizontal", ultimaDireccion.x);
+        animator.SetFloat("LastVertical", ultimaDireccion.y);
     }
 
     void FixedUpdate()
     {
-        // Mover al personaje usando físicas
         rb.MovePosition(rb.position + movimiento.normalized * velocidad * Time.fixedDeltaTime);
     }
 }
